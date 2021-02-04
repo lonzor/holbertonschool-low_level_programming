07ce672f4b93f3afebb361d7cbe9e0f09bf6f4aa
Lonzo Rust <rustlonzo@gmail.com>
2021-02-04 03:11:36 +0000

    Adding file for task 4.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - Prints a message
+ * Return: 0
+ * Description: Program uses puts to print
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
