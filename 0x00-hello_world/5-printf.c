62077e1b416fc058e75231252554f3af2b38fe98
Lonzo Rust <rustlonzo@gmail.com>
2021-02-04 04:29:23 +0000

    Adding file for task 5.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - use printf to print something
+ *
+ * Return: Zero (0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
