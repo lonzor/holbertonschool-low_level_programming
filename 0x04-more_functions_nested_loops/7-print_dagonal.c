6a5ce82ace5814fddbc8db303e5aeeb4dda4ffdf
Lonzo Rust <rustlonzo@gmail.com>
2021-02-11 20:33:33 +0000

    Adding file for task 7.

---
+++ 0x04-more_functions_nested_loops/7-print_dagonal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: how long the diagonal line is
+ * Return: void
+ **/
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	)
+}
