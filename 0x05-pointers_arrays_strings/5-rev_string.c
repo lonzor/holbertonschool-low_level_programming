ad5f6d66711f24b7077e2d6d5b8b76d140e88ab8
Lonzo Rust <rustlonzo@gmail.com>
2021-02-16 21:37:29 +0000

    adding file for task 5.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * rev_string - returns a string reversed
+ * @s: the string being used
+ * Return: void
+ **/
+void rev_string(char *s)
+{
+	int rCount = 0;
+	int fCount = 0;
+	char temp;
+
+	while (s[rCount] != '\0')
+	{
+		rCount++;
+	}
+	rCount--;
+	
+	while (rCount > fCount)
+	{
+		temp = s[fCount];
+		s[rCount] = s[rCount];
+		s[rCount] = temp;
+		rCount--;
+		fCount++;
+	}
+
+}
