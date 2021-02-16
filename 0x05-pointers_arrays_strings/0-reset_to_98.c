4a3ba5e8fce8bc0775a4abc053720a189487e4cf
Lonzo Rust <rustlonzo@gmail.com>
2021-02-16 17:28:07 +0000

    Created directory and file for task 1.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - changes pointer to 98
+ * @n: pointer
+ * Return: nothing
+ **/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory will be used in POINTERS STRINGS and ARRAYS project.
