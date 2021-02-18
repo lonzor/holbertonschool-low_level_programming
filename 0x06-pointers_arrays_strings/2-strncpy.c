6215f44573d6aa504997bc8e95cf68847bd66382
Lonzo Rust <rustlonzo@gmail.com>
2021-02-18 20:58:56 +0000

    Edited task 2..

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * *_strncpy - copies a string
- * @*dest: dest string
- * @*src: source string
+ * _strncpy - copies a string
+ * @dest: dest string
+ * @src: source string
  * @n: chars
  * Return: a copy fo the string
  **/
@@ -11,9 +11,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for(i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	for(; i < n; i++)
+	for (; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
 }
