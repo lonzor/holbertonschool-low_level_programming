0e267aa98dbc05c7e1d135ea143da1a1806949ac
Lonzo Rust <rustlonzo@gmail.com>
2021-02-18 15:22:47 +0000

    Adding file for task 8.

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_array - prints the array
+ * @a: array
+ * @n: how many elements that are in the array
+ * Return: void
+ **/
+void print_arrayyyy(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
