a5c63d492358798959b44e0c985caa0a254ca869
Lonzo Rust <rustlonzo@gmail.com>
2021-02-18 20:56:32 +0000

    Edited task 0.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,27 @@
 #include "holberton.h"
 
 /**
- * *_strcat - concatenates two strings
- * @*dest - the destination string
- * @*src - the source string
+ * _strcat - concatenates two strings
+ * @dest: the destination string
+ * @src: the source string
  * @Return: concatenated strings
  **/
 
 char *_strcat(char *dest, char *src)
 {
 	char* str_ret = dest;
-	
-	if((dest != '\0') && (src != '\0'))
+
+	if ((dest != '\0') && (src != '\0'))
 	{
-		while(*dest != '\0')
+		while (*dest != '\0')
 		{
 			dest++;
 		}
-		while(*src != '\0')
+		while (*src != '\0')
 		{
 			*dest++ = *src++;
 		}
 		*dest = '\0';
 	}
-	return str_ret;
+	return (str_ret);
 }
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * *_strncpy - copies a string
+ * @*dest: dest string
+ * @*src: source string
+ * @n: chars
+ * Return: a copy fo the string
+ **/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for(i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for(; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
