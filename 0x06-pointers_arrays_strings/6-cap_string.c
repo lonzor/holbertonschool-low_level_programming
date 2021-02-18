30c3f5cc11b40c7ad0b8a439b6f4c3152b69b2ea
Lonzo Rust <rustlonzo@gmail.com>
2021-02-18 23:37:18 +0000

    Adding file for task 6.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes each word
+ * @s: string
+ * Return: pointer to the string
+ **/
+char *cap_string(char *s)
+{
+	int i;
+	int char_index;
+
+	char char_array[13] = {' ', '\t', '\n', ',', ';', '.',
+				'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[0] >= 'a' && s[0] <= 'z')
+			s[0] = s[0] - 1;
+		for (char_index = 0; char_index < 13; char_index++)
+		{
+			if (s[i] == char_array[char_index])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+}
