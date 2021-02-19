2b6faf110d9a205356b5b77fa76c33f67d7776db
Lonzo Rust <rustlonzo@gmail.com>
2021-02-19 00:08:13 +0000

    Adding file for task 7.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * leet - translates string to leet
+ * @s: string
+ * Return: pointer to string
+ **/
+char *leet(char *s)
+{
+	int i;
+	int k;
+
+	char norm[10] = {'a', 'A', 'e', 'E', 'o', '0', 't', 'T', 'l', 'L'};
+	char leet[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for(k = 0; k <= 10; k++)
+		{
+			if (s[i] == norm[k])
+				s[i] = leet[k];
+		}
+	}
+	return (s);
+}
