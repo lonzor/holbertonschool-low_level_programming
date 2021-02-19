9a7e5eaee49b9602852acdd6476bc023ee7e1f01
Lonzo Rust <rustlonzo@gmail.com>
2021-02-19 00:21:30 +0000

    Edited task 7.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,12 +10,12 @@ char *leet(char *s)
 	int i;
 	int k;
 
-	char norm[10] = {'a', 'A', 'e', 'E', 'o', '0', 't', 'T', 'l', 'L'};
+	char norm[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char leet[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for(k = 0; k <= 10; k++)
+		for (k = 0; k <= 10; k++)
 		{
 			if (s[i] == norm[k])
 				s[i] = leet[k];
