d3c42d964df4fbe92aa1c8870f5f70947ba3c9c2
Lonzo Rust <rustlonzo@gmail.com>
2021-02-22 19:45:09 +0000

    Created directory and first task file for project 7.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _memset - Fills the first n bytes of the memory
+ * @s: pointer to where bytes are filled
+ * @b: bytes of memory
+ * Return: memory area of s
+ **/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
