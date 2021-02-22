eed2b0fb399936f2ad5208675c4517bf8132f609
Lonzo Rust <rustlonzo@gmail.com>
2021-02-22 19:47:12 +0000

    Forgot to add variable to header comments.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,7 @@
  * _memset - Fills the first n bytes of the memory
  * @s: pointer to where bytes are filled
  * @b: bytes of memory
+ * @n: amount of bytes that will be filled
  * Return: memory area of s
  **/
 char *_memset(char *s, char b, unsigned int n)
