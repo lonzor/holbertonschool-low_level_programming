ecbb98a037abbba9eb7e0597c77606e2a9649268
Lonzo Rust <rustlonzo@gmail.com>
2021-02-23 18:02:50 +0000

    Adding file for task 9.

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets value of a pointer
+ * @s: double pointer to string
+ * @to: pointer to string set to s
+ * Return: returned value description
+ **/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
