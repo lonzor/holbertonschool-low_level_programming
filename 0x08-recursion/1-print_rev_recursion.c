3eda8d783e6d3e671096a108da83660e5b79f0eb
Lonzo Rust <rustlonzo@gmail.com>
2021-02-24 17:08:45 +0000

    Adding file for task 1.

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints the string reversed
+ * @s: string
+ * Return: nothing
+ **/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
