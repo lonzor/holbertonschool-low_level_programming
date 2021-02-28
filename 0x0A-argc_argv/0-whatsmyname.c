8d3bd8cd03592ee0af0dd6a771e074a150355cba
Lonzo Rust <rustlonzo@gmail.com>
2021-02-28 02:03:57 +0000

    Adding file for task 0.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints name
+ * @argc: arg count
+ * @argv: arg array
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
