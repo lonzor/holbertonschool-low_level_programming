4fd84bef42fc340c9bbbf944a47663c19bfcab24
Lonzo Rust <rustlonzo@gmail.com>
2021-03-03 04:46:49 +0000

    Adding files for task 3.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * alloc_grid - creates a pointer to 2D array of int
+ * @width: width of grid
+ * @height: height of grid
+ * Return: pointer to 2D array
+ **/
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int h;
+	int w;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(int) * height);
+	if (grid == NULL)
+	{
+		free(grid);
+		return (NULL);
+	}
+	for (h = 0; h < height; h++)
+	{
+		grid[h] = malloc(sizeof(int) * width);
+		if (grid == NULL)
+		{
+			free(grid);
+			return (NULL);
+		}
+		for (w = 0; w < width; w++)
+		{
+			grid[h][w] = 0;
+		}
+	}
+	return (grid);
+}
