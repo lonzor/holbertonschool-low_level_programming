10e750a6cdbac9e569af37111e13a945f6f64fe9
Lonzo Rust <rustlonzo@gmail.com>
2021-03-03 04:52:06 +0000

    Adding file for task 4.

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid previously created
+ * @grid: the grid given in function
+ * @height: height of grid
+ **/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height < 1)
+		return;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
