a51d348921e78941b3b20957e5ca2a01cba9961e
Lonzo Rust <rustlonzo@gmail.com>
2021-03-08 22:51:49 +0000

    Adding files for task 0 and 1.

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * init_dog - starts the dog creation process
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * @d: dog structure
+ * Return: nothing
+ **/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+        if (d != NULL)
+        {
+                d->name = name;
+                d->age = age;
+                d->owner = owner;
+        }
+}
