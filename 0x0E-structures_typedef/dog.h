93e66f10f0b43519750be75de5a62b07ea0643cb
Lonzo Rust <rustlonzo@gmail.com>
2021-03-08 22:56:36 +0000

    1st edit for task 0.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_DOG_H
+#define _HEADER_DOG_H
 
 /**
  * struct dog - basic dog structure
@@ -15,10 +15,4 @@ struct dog
         float age;
         char *owner;
 };
-typedef struct dog dot_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner(;
-void free_dog(dog_t *d);
 #endif
-
