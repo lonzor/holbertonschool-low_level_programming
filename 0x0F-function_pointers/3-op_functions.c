20ce8e59369caa96baa9b21840935031a515ca86
Lonzo Rust <rustlonzo@gmail.com>
2021-03-11 03:47:43 +0000

    Adding files for task 3.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds int
+ * @a: first int
+ * @b: another int
+ * Return: sum of two ints
+ **/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts int
+ * @a: first int
+ * @b: another int
+ * Return: difference of ints
+ **/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies ints
+ * @a: first int
+ * @b: another int
+ * Return: product of the ints
+ **/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides ints
+ * @a: first int
+ * @b: another int
+ * Return: the result of dividing the ints
+ **/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - gets the remainder
+ * @a: first int
+ * @b: another int
+ * Return: the remainder of the ints
+ **/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
