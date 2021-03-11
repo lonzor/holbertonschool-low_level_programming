9b396af63f778f0aca2e79fc020779407a71255a
Lonzo Rust <rustlonzo@gmail.com>
2021-03-11 05:36:05 +0000

    Corrected error with prototype.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,7 +5,7 @@
  * @s: operator passed as arg to program
  * Return: pointer to function
  **/
-int (*get_op_func(char *s))(int, int);
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
@@ -15,7 +15,9 @@ int (*get_op_func(char *s))(int, int);
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	while (ops[i].op != NULL)
 	{
