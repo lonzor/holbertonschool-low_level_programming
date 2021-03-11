d66a80983443366f71a7c6576d482eb2734bdc9d
Lonzo Rust <rustlonzo@gmail.com>
2021-03-11 04:51:40 +0000

    Changed name and added another file.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+
+/**
+ * get_op_fuc - selects correct function to perform
+ * @s: operator passed as arg to program
+ * Return: pointer to function
+ **/
+int (*get_op_func(char *s))(int, int);
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
