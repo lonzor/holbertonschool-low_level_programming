ea2b07a894f5dfefe3fd66c2d4c8525eb7580efe
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-03-31 15:56:28 +0000

    Changed text_content return to 1.

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	if (!text_content)
-		return (-1);
+		return (1);
 
 	f_open = open(filename, O_RDWR | O_APPEND);
 	if (f_open == -1)
