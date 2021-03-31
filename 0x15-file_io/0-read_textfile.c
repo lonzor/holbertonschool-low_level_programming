79bb7ed7075d7d45d5f9ebc7bce2125389d8dfbe
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-03-31 00:30:34 +0000

    Changed free and null check locations.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,29 +13,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int f_read;
 	int f_write;
 
+	if (filename == NULL)
+		return (0);
 	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL || filename == NULL)
+	if (buff == NULL)
 		return (0);
 
-	f_open = open(filename, O_RDONLY, 0);
+	f_open = open(filename, O_RDONLY);
 	if (f_open == -1)
 	{
-		free(buff);
 		return (0);
 	}
 
 	f_read = read(f_open, buff, letters);
 	if (f_read == -1)
 	{
-		free(buff);
 		return (0);
 	}	
 
 	f_write = write(STDOUT_FILENO, buff, f_read);
-	if (f_read != f_write)
+	if (f_write == -1)
 		return (0);
 
 	free(buff);
 	close(f_open);
-	return (f_read);
+	return (f_write);
 }
