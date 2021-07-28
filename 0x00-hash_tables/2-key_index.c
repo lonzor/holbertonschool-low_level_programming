a6573d2659dcc4931a478d367cba845af2d22ba3
Lonzo Rust <rustlonzo@gmail.com>
2021-07-28 22:34:01 +0000

    Function that gives index of a key.

---
+++ 0x00-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key
+ * @key: they key
+ * @size: size of the array of the hash table
+ * Return: index at the key/value pair
+ **/
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+{
+	unsigned long int kv_index;
+
+	kv_index = hash_djb2(key);
+	kv_index = index % size;
+
+	return (kv_index);
+}
