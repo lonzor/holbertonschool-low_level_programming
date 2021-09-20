901bcb2f779bdd924eccb40cec9a01b719ca550d
Lonzo Rust <rustlonzo@gmail.com>
2021-09-20 17:15:03 +0000

    searches for a value in a sorted array of integers using the Binary search algorithm.

---
+++ 0x02-search_algorithms/1-binary.c
@@ -0,0 +1,62 @@
+#include "search_algos.h"
+/**
+ * binary_search - searches for a value in a sorted array
+ * @array: pointer to first element in array
+ * @size: how many elements that are in the array
+ * @value: what is being searched for
+ * Return: idx where value is located
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t prev;
+	size_t next;
+	size_t curr;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	prev = 0;
+	next = size - 1;
+	print_arr(array, prev, size);
+
+	while(prev < next)
+	{
+		prev = curr + 1;
+		if (array[curr] < value)
+		{
+			prev = curr + 1;
+			print_arr(array, prev, size);
+		}
+		else if (array[curr] > value)
+		{
+			next = curr;
+			print_arr(array, prev, next + 1);
+		}
+		else
+			return (curr);
+	}
+	return (-1);
+}
+
+/**
+ * print_arr - prints array of ints
+ * @array: array that is being printed
+ * @position: idx to starting printing
+ * @size: number of elements in array
+ */
+void print_array(int *array, size_t position, size_t size)
+{
+	size_t = idx;
+
+	printf("Searching in array: ");
+
+	idx = position;
+	while (array && idx < size)
+	{
+		if (idx > position)
+			printf(", ");
+		printf("%d", array[idx]);
+		++idx;
+	}
+	printf("\n");
+}
